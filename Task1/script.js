214c3efd27af25dec4185eaa4dfd6151891f2d75
Olya Belaya <OlyaBelaya84@gmail.com>
2016-12-17 13:01:59 +0200

    first commit

---
+++ Task1/script.js
@@ -0,0 +1,40 @@
+﻿let str ="2016/05/20-12:00:35+0300";
+
+let arrDate = str.match(/\d+\/\d+\/\d+/);
+
+var dateQuery = new Date(arrDate[0]);
+
+let arrTime = str.match(/-(\d+)?:(\d+)?:(\d+)?\+(\d+)/);
+
+console.log(arrTime);
+
+dateQuery.setHours(arrTime[1],arrTime[2],arrTime[3]);
+
+console.log(dateQuery);
+
+let dateTimestamp = dateQuery.getTime();
+
+console.log(dateTimestamp);
+
+
+
+// Так тоже работает
+var today = new Date(str).getTime();
+console.log(today);
+
+
+// Так работает
+var newDate = new Date(2016,04,20, 12, 00, 35).getTime();
+console.log(newDate);
+
+//
+strTest = "2016,05,20";
+var newDate1 = new Date(strTest);
+console.log(newDate1);
+
+// А так не работает
+strTest1 = "2016,05,20, 12, 00, 35";
+var newDate2 = new Date(strTest1);
+console.log(newDate2);
+
+
---
+++ Task2/script.js
@@ -0,0 +1,29 @@
+﻿str = "apple:2016/5/27__bid_203.38-ask_203.43|2016/5/28__bid_203.35-ask_203.42|2016/5/28__bid_203.39-ask_203.45";
+
+let indBegin = str.match(/\w+/);
+let indString = str.match(/:/);
+//console.log(indString);
+
+obj = {
+	stockName: indBegin[0],
+	rates:  []
+}
+
+let arrayObj = str.match(/(\d.+?(\||$))/g);
+let currentStr = "";
+let dateStr = "";
+let bidStr  = "";
+let askStr = "";
+	
+for (let i = 0; i < arrayObj.length; i++){
+	currentStr = arrayObj[i];
+
+	dateStr = currentStr.match(/\d+\/\d+\/\d+/);
+	bidStr = currentStr.match(/(bid_)(\d+\.\d+)/);
+	askStr = currentStr.match(/(ask_)(\d+\.\d+)/);
+
+	obj.rates.push({date:dateStr[0], bid:bidStr[2], ask:askStr[2]});
+
+}
+
+console.log(obj);
